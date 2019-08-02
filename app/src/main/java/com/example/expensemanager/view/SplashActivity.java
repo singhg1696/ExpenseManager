87f21854301d80d16357ebf9f04991aaad643515
singhg1696 <50931443+singhg1696@users.noreply.github.com>
2019-08-02 10:22:03 -0400

    [Gurwinder] Splash Screen Modified

--- app/src/main/java/com/example/expensemanager/view/SplashActivity.java
+++ app/src/main/java/com/example/expensemanager/view/SplashActivity.java
@@ -2,7 +2,9 @@ package com.example.expensemanager.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.expensemanager.R;
 
@@ -11,6 +13,14 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,LoginActivity.class));
+            }
+        },3000);
     }
 }
