bce5b5434dbffcdac6b8d2df6e10b7fcce58833e
singhg1696 <50931443+singhg1696@users.noreply.github.com>
2019-08-02 10:38:09 -0400

    [Gurwinder] Login Views Initialized with Butterknife library

--- app/src/main/java/com/example/expensemanager/view/LoginActivity.java
+++ app/src/main/java/com/example/expensemanager/view/LoginActivity.java
@@ -1,16 +1,36 @@
 package com.example.expensemanager.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.expensemanager.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class LoginActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.txtEmail)
+    TextView txtEmail;
+    @BindView(R.id.txtPass)
+    TextView txtPass;
+    @BindView(R.id.btnLogin)
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(LoginActivity.this);
+    }
+
+    @OnClick(R.id.btnLogin)
+    public void onClick() {
+
     }
 }
