1901a74e7c2f9a1e10b7264aa0b5cde925374ca6
hargunmarya <hargunmarya2726@gmail.com>
2019-08-13 19:33:30 -0400

    [Hargun] Catogries class created

---
+++ app/src/main/java/com/example/expensemanager/Classes/Categories.java
@@ -0,0 +1,24 @@
+package com.example.expensemanager.Classes;
+
+public class Categories {
+    String category;
+
+    @Override
+    public String toString() {
+        return "Categories{" +
+                "category='" + category + '\'' +
+                '}';
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Categories(String category) {
+        this.category = category;
+    }
+}
